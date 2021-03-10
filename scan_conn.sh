059cef9c6804eb92eacebd1172ab352951837f6a
Vitali <31886478+l3m0nqu1z@users.noreply.github.com>
2021-03-10 17:17:29 +0300

    Update scan_conn.sh

--- scan_conn.sh
+++ scan_conn.sh
@@ -2,16 +2,18 @@
 SCRIPTNAME=`basename "$0"`
 if [ "$1" = "" ] || [ "$#" -gt 1 ]
 then
-   echo "(!) Aborting... please run the script with an argument as a searching item e.g."
-   echo "    ./$SCRIPTNAME firefox -----> PROCESS NAME"
-   echo "    ./$SCRIPTNAME 3123 --------> PID"
-   echo "    ./$SCRIPTNAME ESTAB -------> STATE ( ESTAB | LISTEN | UNCONN )"
-   echo "    ./$SCRIPTNAME tcp ---------> Netid ( tcp | udp )"
-   exit 1
+cat << ALERT
+(!) Aborting... please run the script with an argument as a searching item e.g.
+./$SCRIPTNAME firefox -----> PROCESS NAME
+./$SCRIPTNAME 3123 --------> PID
+./$SCRIPTNAME ESTAB -------> STATE ( ESTAB | LISTEN | UNCONN )
+./$SCRIPTNAME tcp ---------> Netid ( tcp | udp )
+ALERT
+exit 1
 else
    PROCESS=$1
 fi
-if ! command -v ss >> /dev/null 2>&1 ||
+if ! command -v ss >> /dev/null 2>&1 || 
    ! command -v whois >> /dev/null 2>&1
 then
    echo "Firstly, there is need to install 'ss' and 'whois' on your PC"
@@ -19,17 +21,16 @@ then
    read answer
    case $answer in
       Y|y|Yes|yes|"")
-        echo "Installing components..."
-        sudo apt update -y >> /dev/null 2>&1
-        sudo apt install -y ss-dev \
-                       whois >> /dev/null 2>&1
-        echo "Done. Now script is working..."
-        sleep 1
-        ;;
+         echo "Installing components..."
+         sudo apt update -y >> /dev/null 2>&1
+         sudo apt install -y ss-dev whois >> /dev/null 2>&1
+         echo "Done. Now script is working..."
+         sleep 1
+         ;;
       *)
-        echo "(!) Aborting..."
-        exit
-        ;;
+         echo "(!) Aborting..."
+         exit
+         ;;
    esac
 fi
 TMP=$(date +"%H%M%S".ss)
@@ -43,30 +44,29 @@ cut -d: -f 1 | sort | uniq -c | sort | awk '{print $2}' | tac
 }
 results() {
 head -n $1 $TMPPATH | while read IP
-   do
-     local res=$(whois $IP | awk -F':' '/^Organization/ {print $2}')
-     [ -n "$res" ] && echo "$res" && echo "$res" >> $TMPPATH.org
-     [ -z "$res" ] && echo "(!) Cannot find Organization of IP: $IP"
-   done
-   if [ -f $TMPPATH.org ]
-   then
-     echo "_________________________________________________________"
-     echo "Connections per Org: "
-     cat $TMPPATH.org | sort | uniq -c
-     > $TMPPATH.org
-   fi
+do
+   local res=$(whois $IP | awk -F':' '/^Organization/ {print $2}')
+   [ -n "$res" ] && echo "$res" && echo "$res" >> $TMPPATH.org
+   [ -z "$res" ] && echo "(!) Cannot find Organization of IP: $IP"
+done
+if [ -f $TMPPATH.org ]; then
+   echo "_________________________________________________________"
+   echo "Connections per Org: "
+   cat $TMPPATH.org | sort | uniq -c
+   > $TMPPATH.org
+fi
 }
 more_results() {
 echo -n "To see more results press 'l' or any other key to exit: "
 read more_results
 case $more_results in
    l)
-    echo "###########################################################"
-    NUM_RES=$(($NUM_RES+10))
-    results $NUM_RES
+      echo "###########################################################"
+      NUM_RES=$(($NUM_RES+10))
+      results $NUM_RES
    ;;
    *)
-   exit
+      exit
    ;;
 esac
 }
